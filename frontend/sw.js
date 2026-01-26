57f74c7d23aaade3b73f32ebd58c16ec34deb579
shihanastephanii <steph.shihana@gmail.com>
2026-01-26 11:48:16 +0530

    created frontend folder

---
+++ frontend/sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = "budgetai-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
